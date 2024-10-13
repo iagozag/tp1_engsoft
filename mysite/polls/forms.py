2e1ca27eae309cab6347d51f9fdc0c5d060f9c62
iagozag <iagozag@gmail.com>
2024-10-13 00:12:07 -0300

    finalizando completar cadastro

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -5,7 +5,7 @@ class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ['email', 'senha']
-    
+
 
 class CompletaForm(forms.ModelForm):
     class Meta:
@@ -16,13 +16,12 @@ class CompletaForm(forms.ModelForm):
         required=False,
         label="Você é um motorista?"
     )
-    
+
 class VeiculoForm(forms.ModelForm):
     class Meta:
         model = Veiculo
-        fields = ['placa','modelo', 'cor'] 
-   
+        fields = ['placa','modelo', 'cor']
+
 class LoginForm(forms.Form):
     email = forms.CharField(label = "Email", max_length=50)
     senha = forms.CharField(widget=forms.PasswordInput)
-        
