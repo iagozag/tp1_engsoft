1083e8d1856776be1087f379ca13367a3a5b6296
debyyamamoto <yamamotodeborah@gmail.com>
2024-10-18 08:43:57 -0300

    primeira versão da visualização de caronas

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('login/', views.login_usuario, name = 'login'),
     path('configuracoes/',views.configuracoes,name='configuracoes'),
     path('criar_carona/',views.criar_carona,name='criar_carona'),
-    #path('visualizar_caronas/',)
+    path('visualizar_caronas/', views.visualizar_caronas, name='visualizar_caronas'),
 ]
